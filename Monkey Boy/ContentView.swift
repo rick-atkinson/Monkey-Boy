81b56ca2a26855740466d2e5112fafb3e3bc3926
Rick Atkinson <68563439+rick-atkinson@users.noreply.github.com>
2026-01-21 10:10:13 -0500

    Initial Commit

---
+++ Monkey Boy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Monkey Boy
+//
+//  Created by Rick Atkinson on 1/21/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Monkey Boy/Monkey_BoyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Monkey_BoyApp.swift
+//  Monkey Boy
+//
+//  Created by Rick Atkinson on 1/21/26.
+//
+
+import SwiftUI
+
+@main
+struct Monkey_BoyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
